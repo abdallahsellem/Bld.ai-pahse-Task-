b9773635735e9393e2397bd9fd7d1cd855a49e08
abdallah selim <abdallahmselim@gmail.com>
2022-08-14 21:14:16 +0200

    Making Tabs Workable , Adding Sliding Cards

--- javascript.js
+++ javascript.js
@@ -1,42 +1,54 @@
 let GlobalInput = "   ";
+let target = "courses_python";
+let IsEvent = false;
 function search_animal() {
   let input = document.getElementById("inpooo").value;
   input = input.toLowerCase();
-  if (GlobalInput === input) {
+
+  if (GlobalInput === input && IsEvent === false) {
     return;
   }
+  console.log(IsEvent);
+  IsEvent = false;
   GlobalInput = input;
   const renderDetails = async () => {
-    // console.log("ahemd") ;
-    const res = await fetch("http://localhost:3000/courses");
+    console.log(target);
+    const res = await fetch("http://localhost:3000/" + target);
     const post = await res.json();
     Listi = post;
     templete = "";
-    let Big_div = document.querySelector(".Courses_Section");
-    Big_div.innerHTML = "";
-    //Big_div.removeChild();
+    let Big_div = document.querySelectorAll(".CardWarp");
+    let Curr_Div = Big_div[0];
+    Big_div[0].innerHTML = "";
+    Big_div[1].innerHTML = "";
+    let NumberOfCards = 0;
     for (i = 0; i < Listi.length; i++) {
+      if (NumberOfCards >= 3) {
+        Curr_Div = Big_div[1];
+      }
       a = Listi[i]["title"];
-
       if (a.toLowerCase().indexOf(input) > -1) {
-        let DivOfCourse = document.createElement("div");
-        DivOfCourse.className = "Con2Div";
-        let imgOfDiv = document.createElement("img");
-        imgOfDiv.src = Listi[i]["image"];
-        DivOfCourse.appendChild(imgOfDiv);
-        let DivOfTitle = document.createElement("div");
-        DivOfTitle.className = "CoursesB1";
-        Header3 = document.createElement("h3");
-        Header3.textContent = a;
-        DivOfTitle.appendChild(Header3);
-        DivOfCourse.appendChild(DivOfTitle);
+        NumberOfCards++;
+        Card = document.createElement("div");
+        Card.className = "card";
+        Imgi = document.createElement("img");
+        Imgi.src = Listi[i]["image"];
+        Imgi.className = "card-img-top";
+        Imgi.alt = "...";
+        Card.appendChild(Imgi);
+        BodyOfCard = document.createElement("div");
+        BodyOfCard.className = "card-body";
+        CardTitle = document.createElement("h5");
+        CardTitle.textContent = a;
+        CardPrice = document.createElement("h6");
+        CardPrice.textContent = Listi[i]["price"];
         SpanOfRating = document.createElement("span");
         SpanOfRating.className =
           "udlite-heading-sm StarRating--rating-number--2o8YM";
         SpanOfRating.ariaHidden = "hidden";
         SpanOfRating.dataset.purpose = "rating-number";
         SpanOfRating.textContent = Listi[i]["rating"];
-        DivOfCourse.appendChild(SpanOfRating);
+        BodyOfCard.appendChild(SpanOfRating);
         SecOfStars = document.createElement("section");
         SecOfStars.className = "StarRating";
         for (let i = 1; i <= 4; i++) {
@@ -45,23 +57,32 @@ function search_animal() {
           SecOfStars.appendChild(stars);
         }
         ancOfstars = document.createElement("a");
-        ancOfstars.textContent = Listi[i]["people"];
+        ancOfstars.textContent = "(" + Listi[i]["people"] + ")";
         SecOfStars.appendChild(ancOfstars);
-        DivOfCourse.appendChild(SecOfStars);
-        DivOfPrice = document.createElement("div");
-        DivOfPircechild = document.createElement("div");
-        DivOfPircechild.className = "PriceText2";
-        SpanOfPrice = document.createElement("span");
-        SpanOfPrice.textContent = Listi[i]["price"];
-        DivOfPircechild.appendChild(SpanOfPrice);
-        DivOfPrice.appendChild(DivOfPircechild);
-        DivOfCourse.appendChild(DivOfPrice);
-
-        //BIG DIV SCOPE
-        Big_div.appendChild(DivOfCourse);
+        BodyOfCard.appendChild(SecOfStars);
+        BodyOfCard.appendChild(CardTitle);
+        BodyOfCard.appendChild(CardPrice);
+        Card.appendChild(BodyOfCard);
+        Curr_Div.appendChild(Card);
       }
     }
   };
   renderDetails();
 }
 setInterval(search_animal, 1000);
+document.querySelector(".python").addEventListener("click", () => {
+  target = "courses_python";
+});
+document.querySelector(".Web").addEventListener("click", () => {
+  target = "courses_Web";
+  IsEvent = true;
+});
+document.querySelector(".Java").addEventListener("click", () => {
+  target = "courses_Java";
+  IsEvent = true;
+});
+document.querySelector(".excel").addEventListener("click", () => {
+  target = "courses_excel";
+  IsEvent = true;
+  //console.log(target);
+});
